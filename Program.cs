0e1d4357e7fc23a979ad840795adc58dab6e9d85
BoonMode <69335910+BoonMode@users.noreply.github.com>
2020-08-15 16:16:19 +0800

    Hello world for C Sharp.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace c_Sharp_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
